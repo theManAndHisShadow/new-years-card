b16e7fdc3092fd2275d9ffeb6686162317f4297c
theManAndHisShadow <justemailpostbox@gmail.com>
2024-12-31 20:00:09 +0500

    [Scene]: added first scene firework code.

--- js/code.js
+++ js/code.js
@@ -3,7 +3,7 @@ function createApp(params) {
     // init root node
     const root = document.querySelector(params.root);
 
-    if(!root) {
+    if (!root) {
         throw new Error('Root node is not exist!');
     }
 
@@ -11,7 +11,7 @@ function createApp(params) {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
 
-    if(!context) {
+    if (!context) {
         throw new Error('Context is not exist!');
     }
 
@@ -22,22 +22,35 @@ function createApp(params) {
     canvas.width = width;
     canvas.height = height;
 
+    const isAnimated = params.loop || false;
+
     // inject app canvas to app root
     root.appendChild(canvas);
 
     // return app instance
     return {
         renderer: context,
-        body: canvas,
+        screen: canvas,
         bounds: {
-            leftTop: {x: 0, y: 0},
-            rightTop: {x: width, y: 0},
-            rightBottom: {x: width, y: height},
-            leftBottom: {x: 0, y: height},
+            leftTop: { x: 0, y: 0 },
+            rightTop: { x: width, y: 0 },
+            rightBottom: { x: width, y: height },
+            leftBottom: { x: 0, y: height },
         },
 
-        execute: function(code){
-            code(this);
+        loop: isAnimated,
+
+        execute: function (code) {
+            if (isAnimated) {
+                let loopFn = () => {
+                    requestAnimationFrame(loopFn);
+                    code(this);
+                };
+
+                requestAnimationFrame(loopFn);
+            } else {
+                code(this);
+            }
         },
     }
 }
@@ -48,9 +61,31 @@ let app = createApp({
     root: '#app-root',
     width: 330,
     height: 430,
+    loop: true,
 });
+
+let firework = new Firework({
+    launchPoint: { x: app.screen.width / 2, y: app.screen.height },
+    blastPoint: { x: app.screen.width / 2, y: (app.screen.height / 2) - 100 },
+    areaWidth: 300,
+    areaHeight: 300,
+    blastPower: 100,
+    colors: ['#FF0000', '#00FF00', '#0000FF', '#FFFF00', '#FF00FF'],
+});
+
+firework.appendTo(app);
 
 // add some code
 app.execute(app => {
+    // fill with color
+    const color = '#0b1026';
+    app.renderer.fillStyle = color;
+    app.renderer.fillRect(
+        app.bounds.leftTop.x,
+        app.bounds.leftTop.y,
+        app.bounds.rightBottom.x,
+        app.bounds.rightBottom.y
+    );
 
+    firework.launch();
 });
